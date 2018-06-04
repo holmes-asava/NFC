cf75ca8f62b678114e8e1e083affb90ef579589c
Unknown <homer.asava@gmail.com>
2018-06-04 11:12:50 +0800

    test signal and slots

--- NFC_monitoring/mainwindow.cpp
+++ NFC_monitoring/mainwindow.cpp
@@ -1,14 +1,47 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    QMainWindow(parent)
 {
-    ui->setupUi(this);
+        window  = new QWidget;
+        port    = new QLabel("Port: none");
+        status  = new QLabel("Status: Disconnect");
+        button1 = new QPushButton("Connect",this);
+        button2 = new QPushButton("Start");
+        button3 = new QPushButton("Stop");
+        connect(button1, SIGNAL (released()), this, SLOT (handlelCon()));
+        portlayout  = new QHBoxLayout ;
+        statuslayout= new QHBoxLayout ;
+        buttonlayout= new QHBoxLayout ;
+
+
+        portlayout->addWidget(port);
+        statuslayout->addWidget(status);
+        buttonlayout->addWidget(button1);
+        buttonlayout->addWidget(button2);
+        buttonlayout->addWidget(button3);
+        settinglayout=new QGridLayout ;
+        settinglayout->addLayout(portlayout,0,0);
+        settinglayout->addLayout(statuslayout,1,0);
+        settinglayout->addLayout(buttonlayout,2,0);
+
+        window->setLayout(settinglayout);
+        window->show();
+
+
+
+
 }
 
-MainWindow::~MainWindow()
+void MainWindow::handlelCon()
 {
-    delete ui;
+   // change the text
+   button1->setText("Example");
+   // resize button
+
+
 }
+
+
+
--- NFC_monitoring/mainwindow.h
+++ NFC_monitoring/mainwindow.h
@@ -2,7 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include<QWidget>
+#include<QPushButton>
+#include<QHBoxLayout>
+#include<QGridLayout>
+#include<QLabel>
+#include<QLineEdit>
 namespace Ui {
 class MainWindow;
 }
@@ -13,10 +18,25 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+private slots:
+    void handlelCon();
 
 private:
-    Ui::MainWindow *ui;
+    QLabel *port;
+    QWidget *window;
+    QLabel *status;
+
+    QPushButton *button1;
+    QPushButton *button2;
+    QPushButton *button3;
+
+
+    QHBoxLayout *portlayout ;
+    QHBoxLayout *statuslayout ;
+    QHBoxLayout *buttonlayout ;
+
+    QGridLayout *settinglayout ;
+
 };
 
 #endif // MAINWINDOW_H
